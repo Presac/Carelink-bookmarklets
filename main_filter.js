99e901308b4ebad28d5fe3fb3e63568eb5cdf78e
Presac <casperhahn@hotmail.com>
2021-05-18 22:53:49 +0200

    changed to hyphen

--- main_filter.js
+++ main_filter.js
@@ -16,29 +16,30 @@ function containsObject(obj, list) {
     return false;
 };
 
-var vagter = document.querySelectorAll("tr[id^=vagtrow_booket_]");
+var vagter = document.querySelectorAll('tr[id^=vagtrow_booket_]');
 var entries = [];
 var hideExtra = false;
 Array.from(
     vagter.forEach(function (y) {
-        if (y.id.indexOf("extra") > -1) {
+        if (y.id.indexOf('extra') > -1) {
             if (hideExtra) {
-                y.style.display = "none";
+                y.style.display = 'none';
                 hideExtra = false;
             };
             return;
         };
         var info = {
-            "dato": y.children[0].textContent,
-            "tid": y.children[1].textContent,
-            "timer": y.children[2].textContent,
-            "uddanelse": y.children[3].textContent,
-            "kunde": y.children[4].textContent
+            'dato': y.children[0].textContent,
+            'tid': y.children[1].textContent,
+            'timer': y.children[2].textContent,
+            'uddanelse': y.children[3].textContent,
+            'kunde': y.children[4].textContent,
+            'info': y.children[5].textContent
         };
-        // add" || info["kunde"].includes("location") != true " for
+        // add' || info['kunde'].includes('location') != true ' for
         // whatever place you want to remove from the list
-        if (containsObject(info, entries) || info["kunde"].includes("Odense") != true) {
-            y.style.display = "none";
+        if (containsObject(info, entries) || info['kunde'].includes('Odense') != true) {
+            y.style.display = 'none';
             hideExtra = true;
         } else {
             entries.push(info);
